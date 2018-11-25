13256777d4931788aabc8b34a3771f1a9393f532
Jiang  Zheping <jiangz32@ug220.eecg.toronto.edu>
2018-11-25 15:29:43 -0500

    Add modified makefile and codegen files

---
+++ compiler467/lab4/codegen.h
@@ -0,0 +1,16 @@
+/***********************************************************************
+ * codegen.h
+ *
+ * Lab4: Code Generation
+ **********************************************************************/
+
+#ifndef _CODEGEN_H_ 
+#define _CODEGEN_H_
+
+#include <stdio.h> /* for FILE */
+#include "ast.h"
+
+void genCode(node *ast);
+
+
+#endif
