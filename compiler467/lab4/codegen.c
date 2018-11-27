f898e1ad9fb7a6bdd069c3ead95f843c089a7914
Shifan Gu <gushifan@gmail.com>
2018-11-27 15:51:01 -0500

    Add ast node to register allocation prototype

--- compiler467/lab4/codegen.c
+++ compiler467/lab4/codegen.c
@@ -1,14 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "codegen.h"
-
+#include "ast.h"
 
 /**********************************************************************
- * Helper functions go here
+ * Global stuffs go here
  *********************************************************************/ 
+// Function declarations
+typedef enum reg_type {
+    UNKNOWN_REG_TYPE = 3056,
+    PARAM_REG_TYPE = 3057,
+    TEMP_REG_TYPE = 3058,
+    GLOB_REG_TYPE = 3059        
+} reg_type;
 
+// Global structures
+typedef struct ast_register {
+    struct ast_register* next;
+    node* ast_node;
+    char* reg_name;
+    enum reg_type type;
+} reg;
 
+reg* get_register(node* ast_node) {
+    return NULL;
+}
 
+//void generate_reg_name(reg* theReg, bool is) {
+//    if (!theReg || ! regName) return;
+//    theReg->reg_name = strdup(regName);
+//    return;
+//}
+
+char* get_predef_reg_name_by_id(char* id) {
+    char* regName = NULL;
+    if (!id) return regName;
+    if (!strcmp(id, "gl_FragColor")) {
+        regName = strdup("result.color");
+    } else if (!strcmp(id, "gl_FragDepth")) {
+        regName = strdup("result.depth");
+    } else if (!strcmp(id, "gl_FragCoord")) {
+        regName = strdup("fragment.position");
+    } else if (!strcmp(id, "gl_TexCoord")) {
+        regName = strdup("fragment.texcoord");
+    } else if (!strcmp(id, "gl_Color")) {
+        regName = strdup("fragment.color");
+    } else if (!strcmp(id, "gl_Secondary")) {
+        regName = strdup("fragment.color.secondary");
+    } else if (!strcmp(id, "gl_FogFragCoord")) {
+        regName = strdup("fragment.fogcoord");
+    } else if (!strcmp(id, "gl_Light_Half")) {
+        regName = strdup("state.light[0].half");
+    } else if (!strcmp(id, "gl_Light_Ambient")) {
+        regName = strdup("state.lightmodel.ambient");
+    } else if (!strcmp(id, "gl_Material_Shininess")) {
+        regName = strdup("state.material.shininess");
+    } else if (!strcmp(id, "env1")) {
+        regName = strdup("program.env[1]");
+    } else if (!strcmp(id, "env2")) {
+        regName = strdup("program.env[2]");
+    } else if (!strcmp(id, "env3")) {
+        regName = strdup("program.env[3]");
+    }
+    return regName;
+}
+
+// Global variables
 /**********************************************************************
  * Lab 4: Code Generation
  *********************************************************************/ 
